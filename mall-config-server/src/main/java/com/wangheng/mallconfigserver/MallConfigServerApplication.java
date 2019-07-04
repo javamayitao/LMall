60cbcf0957485bf7be8285d31c20cf5cab180459
wangheng <1012056369@qq.com>
2019-07-04 16:33:02 +0800

    新增配置模块

---
+++ mall-config-server/src/main/java/com/wangheng/mallconfigserver/MallConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.wangheng.mallconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MallConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallConfigServerApplication.class, args);
+    }
+
+}
